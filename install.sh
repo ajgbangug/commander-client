ed77988fa397832e9cfd462c0fb2e845280faf11
AJ Bangug <ajgbangug@gmail.com>
2013-04-13 11:30:48 +0800

    modified the install script

--- install.sh
+++ install.sh
@@ -4,7 +4,3 @@ dpkg -i puppetlabs-release-quantal.deb
 apt-get update -y
 apt-get install -y build-essential python-dev python-pip facter libjson-ruby openssh-server
 pip install pymongo
-wget https://github.com/ajgbangug/commander-client/archive/master.zip
-unzip master.zip
-mkdir /opt/commander
-ln -s /home/commander/commander-client-master/client.py /opt/commander/
